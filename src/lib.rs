30d486de884b80c878045598dba09a9a68383be1
wattlebirdaz <wattlebirdaz@gmail.com>
2024-07-15 16:20:29 -0500

    fix include

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
+pub mod append_only_store;
 pub mod bench_utils;
 pub mod bp;
 pub mod fbt;
+pub mod random;
 pub mod txn_storage;
 pub mod write_ahead_log;
 
-mod append_only_store;
 mod file_manager;
 mod heap_page;
 mod logger;
 mod page;
-pub mod random;
 mod rwlatch;
 
 pub use logger::log;
